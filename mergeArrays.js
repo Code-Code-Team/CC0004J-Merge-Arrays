93c63e5dd30554fb66cc0c8a199e54187d829e64
E2029-Aydin <aydinsanoz2@gmail.com>
2020-10-13 15:17:40 +0300

    enter event added

---
+++ mergeArrays.js
@@ -0,0 +1,80 @@
+
+const firstInputElement = document.querySelector('#firstInput');
+// console.log("firstInputElement", firstInputElement)
+const secondInputElement = document.querySelector('#secondInput');
+// console.log("secondInputElement", secondInputElement)
+const firstInputButton = document.querySelector('#firstInputButton');
+const secondInputButton = document.querySelector('#secondInputButton');
+const concatInputButton = document.querySelector('#concatInputButton');
+const resetButton = document.querySelector('#resetAllButton');
+const firstArrayText = document.querySelector('#firstArrayText');
+const secondArrayText = document.querySelector('#secondArrayText');
+const mergedArrayText = document.querySelector('#mergedArrayText');
+
+
+
+const firstArray = [];
+const secondArray = [];
+
+function addToFirst(){
+    if(firstInputElement.value != ''){
+        firstArray.push(firstInputElement.value);
+        firstInputElement.value = "";
+        firstArrayText.innerText = `FirstArray : ${firstArray.join(' ')}`;
+    }
+    firstInputElement.focus();
+}
+function addToSecond(){
+    if(secondInputElement.value != ''){
+        secondArray.push(secondInputElement.value);
+        secondInputElement.value = "";
+        secondArrayText.innerText = `SecondArray : ${secondArray.join(' ')}`;
+    }
+    secondInputElement.focus();
+}
+
+
+firstInputButton.addEventListener('click', (addToFirst));
+firstInputElement.addEventListener('keyup', (e) =>{
+    if(e.keyCode == 13){
+        addToFirst();
+    }
+})
+secondInputButton.addEventListener('click',(addToSecond))
+secondInputElement.addEventListener('keyup', (e) =>{
+    if(e.keyCode == 13){
+        addToSecond();
+    }
+})
+
+//TODO : Reset button add
+resetButton.addEventListener('click',()=>{
+    firstArray = "";
+    secondArray = "";
+})
+
+
+
+concatInputButton.addEventListener('click', () =>{
+    const mergedArray =(firstArray.concat(secondArray).sort().join(' '))
+    mergedArrayText.innerText = `MergedArray : ${mergedArray}`
+   
+})
+
+
+
+
+
+function mergeArray(){
+
+    
+    // [2,3,5,2,7,9]
+    // ['a','b','C22', 'xx']
+    firstArray.push(firstInputElement.value);
+    // const secondArray = secondArray.push(secondInputElement.value);
+    
+    
+    // mergedArray = (firstArray.concat(secondArray)).sort();
+    // console.log("mergedArray -> mergedArray", mergedArray)
+}
+
